28fce35937f00b43f1f042598a05522ce28d359b
Johandielangman <jghanekom2@gmail.com>
2024-08-02 20:24:11 +0200

    fix: create dirs if they don't exist

--- src/02_config.R
+++ src/02_config.R
@@ -20,6 +20,14 @@ OUTPUT_DIR <- "output"
 OUTPUT_DATA_DIR <- file.path(OUTPUT_DIR, "data")
 OUTPUT_PLOTS_DIR <- file.path(OUTPUT_DIR, "plots")
 
+ALL_DIRS <- c(DATA_PROCESSED_DIR, DATA_RAW_DIR, OUTPUT_DATA_DIR, OUTPUT_PLOTS_DIR)
+for (dir in ALL_DIRS){
+  if (!dir.exists(dir)){
+    message("Creating directory: ", dir)
+    dir.create(dir, recursive = TRUE)
+  }
+}
+
 UTILS_DIR <- file.path(SCRIPT_DIR, "utils")
 
 # -----------------// ADDITIONAL SETUP (E.G. THEMING) //-----------------
